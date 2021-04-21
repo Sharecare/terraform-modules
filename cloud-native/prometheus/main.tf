5bd58088ea4d64204ff3d256409e1b52cd50324e
bluehawk27 <avillacorta27@gmail.com>
2021-04-21 15:57:02 -0700

    clean up

--- cloud-native/prometheus/main.tf
+++ cloud-native/prometheus/main.tf
@@ -1,9 +1,9 @@
 ###################### PROMETHEUS
 resource "helm_release" "prometheus" {
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  # version          = "14.9.0"
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "15.1.2"
   namespace        = "prometheus"
   create_namespace = true
   wait             = true
@@ -14,21 +14,3 @@ resource "helm_release" "prometheus" {
     templatefile("./${path.module}/templates/prometheus.values.yaml", {})
   ]
 }
-
-###################### GRAFANA
-
-# resource "helm_release" "grafana" {
-#   name       = "grafana"
-#   repository = "https://grafana.github.io/helm-charts"
-#   chart      = "grafana"
-#   # version          = "14.9.0"
-#   namespace        = "prometheus"
-#   create_namespace = true
-#   wait             = true
-#   recreate_pods    = true
-#   lint             = true
-
-#   values = [
-#     templatefile("./${path.module}/templates/prometheus.values.yaml", {})
-#   ]
-# }
