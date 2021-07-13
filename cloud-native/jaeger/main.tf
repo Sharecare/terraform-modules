ec9522863c4cf5904cff898c22e71bc1e91163e9
Kyle Helle <kyle.helle@sharecare.com>
2021-07-13 15:48:37 -0700

    Add Jaeger tf module

---
+++ cloud-native/jaeger/main.tf
@@ -0,0 +1,16 @@
+###################### JAEGER
+resource "helm_release" "jaeger" {
+  name             = "jaeger"
+  repository       = "https://jaegertracing.github.io/helm-charts"
+  chart            = "jaegertracing/jaeger"
+  version          = "0.46.4"
+  namespace        = "jaeger"
+  create_namespace = true
+  wait             = true
+  recreate_pods    = true
+  lint             = true
+
+  values = [
+    templatefile("./${path.module}/templates/jaeger.values.yaml", {})
+  ]
+}
