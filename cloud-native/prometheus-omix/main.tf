b32bc3cd012c5bc75529ba0d85c3918caf74582a
bluehawk27 <alberto@doc.ai>
2022-01-03 11:06:05 -0800

    create prometheus modules for toniq/omix

---
+++ cloud-native/prometheus-omix/main.tf
@@ -0,0 +1,31 @@
+###################### PROMETHEUS
+resource "helm_release" "prometheus" {
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "20.0.1"
+  namespace        = "prometheus"
+  create_namespace = true
+  wait             = true
+  recreate_pods    = false
+  lint             = true
+
+  values = [
+    templatefile("./${path.module}/templates/prometheus.values.yaml", {
+      ALLOWED_DOMAINS      = var.allowed_domains,
+      GOOGLE_CLIENT_ID     = var.google_client_id,
+      GOOGLE_CLIENT_SECRET = var.google_client_secret,
+      GRAFANA_URL          = var.grafana_url
+      GRAFANA_ENABLED      = var.grafana_enabled
+      CLUSTER_NAME         = var.cluster_name
+    })
+  ]
+
+  dynamic "set" {
+    for_each = var.values_override
+    content {
+      name  = set.key
+      value = set.value
+    }
+  }
+}
