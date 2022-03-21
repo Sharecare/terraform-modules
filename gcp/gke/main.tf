a2dbe7671bb7796ccb522414156dc37d58beb854
bluehawk27 <alberto@doc.ai>
2022-03-21 16:02:04 -0700

    bump gke version

--- gcp/gke/main.tf
+++ gcp/gke/main.tf
@@ -1,6 +1,6 @@
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
-  version                    = "17.3.0"
+  version                    = "20.0.0"
   project_id                 = var.project_id
   name                       = "${var.project_id}-cluster"
   region                     = var.region
