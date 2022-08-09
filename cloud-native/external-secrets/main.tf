a379dfefc876e0057aadf69693fad40a5a2b71f0
bluehawk27 <alberto@doc.ai>
2022-08-09 12:29:53 -0700

    update external secrets module

--- cloud-native/external-secrets/main.tf
+++ cloud-native/external-secrets/main.tf
@@ -1,70 +1,32 @@
-resource "null_resource" "k8_service_account" {
-  triggers = { always_run = timestamp() }
-  provisioner "local-exec" {
-    command = "gcloud container clusters describe  $CLUSTER --project $PROJECT_ID --region $REGION | grep \"serviceAccount\" | cut -d\":\" -f2 | sort -u | xargs | head -1 > ${path.module}/k8_service_account.txt"
+resource "helm_release" "external-secrets" {
 
-    environment = {
-      CLUSTER    = var.cluster
-      REGION     = var.region
-      PROJECT_ID = var.project_id
+  name                  = "external-secrets"
+  repository            = "https://charts.external-secrets.io"
+  chart                 = "external-secrets/external-secrets"
+  version               = "0.5.9"
+  namespace             = var.namespace
+  create_namespace      = true
+  wait                  = true
+  lint                  = true
+  render_subchart_notes = true
+  cleanup_on_fail       = true
+
+  values = [file("${path.module}/templates/values.yaml")]
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = var.service_account_name
+  }
+  dynamic "set" {
+    for_each = var.values_overrides
+    content {
+      name  = set.key
+      value = set.value
     }
   }
-
-}
-
-data "local_file" "service_account_file" {
-  filename   = "${path.module}/k8_service_account.txt"
-  depends_on = [null_resource.k8_service_account]
-}
-
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/iam.workloadIdentityUser"
-
-    members = [
-      "serviceAccount:${var.project_id}.svc.id.goog[${var.namespace}/${var.k8_service_account}]",
-    ]
-  }
-}
-
-resource "google_service_account_iam_policy" "admin-account-iam" {
-  service_account_id = "projects/${var.project_id}/serviceAccounts/${trimspace(data.local_file.service_account_file.content)}"
-  policy_data        = data.google_iam_policy.admin.policy_data
-}
-
-resource "google_project_iam_binding" "project" {
-  project = var.project_id
-  role    = "roles/secretmanager.secretAccessor"
-  members = [
-    "serviceAccount:${trimspace(data.local_file.service_account_file.content)}",
-  ]
-}
-
-data "template_file" "template_values_file" {
-  template = file("${path.module}/templates/values.tpl")
-  vars = {
-    gcp_service_account = trimspace(data.local_file.service_account_file.content)
-    k8_service_account  = var.k8_service_account
-  }
-}
-
-resource "helm_release" "externalsec" {
-
-  name = "externalsec"
-  #repository       = "https://external-secrets.github.io/kubernetes-external-secrets"
-  chart            = "external-secrets/kubernetes-external-secrets"
-  version          = "6.1.0"
-  namespace        = var.namespace
-  create_namespace = true
-  wait             = true
-
-  values = [
-    data.template_file.template_values_file.rendered
-  ]
-}
-
-resource "null_resource" "done" {
-  depends_on = [
-    helm_release.externalsec
-  ]
 }
--- cloud-native/external-secrets/variables.tf
+++ cloud-native/external-secrets/variables.tf
@@ -1,33 +1,10 @@
-
-variable "project_id" {
-  type        = string
-  description = "GCP project"
-}
-
-variable "cluster" {
-  type        = string
-  description = "GCP kubernetes cluster "
-}
-
-
-
-variable "region" {
-  type        = string
-  description = "region where the kubernetes cluster is deployed"
-}
-
-variable "requirements" {
-  type = list(string)
-}
-
-variable "k8_service_account" {
-  type        = string
-  description = "service account in k8 cluster with secret access privelages "
-  default     = "external-secrets"
-}
-
 variable "namespace" {
   type        = string
   description = "namespace where the external secrets in deployed"
-  default     = "default"
+  default     = "external-secrets"
+}
+
+variable "service_account_name" {
+  type        = string
+  description = "name of the k8s service account running with secret access privelages Required for external secrets"
 }
