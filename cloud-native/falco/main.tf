b146cc3e0df4ed1bc37a1f7889e201f8d09fbe4c
Justin Disney <justin.disney@share.com>
2024-10-17 18:57:40 +0000

    [SITEOPS-113928] update falco helm chart to 4.11.1. Remove openfaas and falcosidekick resources (sidekick is now managed as part of the falco helm chart)

--- cloud-native/falco/main.tf
+++ cloud-native/falco/main.tf
@@ -5,7 +5,7 @@ resource "helm_release" "falco" {
   name             = "falco"
   repository       = "https://falcosecurity.github.io/charts"
   chart            = "falco"
-  version          = "2.0.5"
+  version          = "4.11.1"
   namespace        = "falco"
   create_namespace = true
   wait             = true
@@ -17,6 +17,22 @@ resource "helm_release" "falco" {
     data.template_file.falco_template.rendered,
     data.template_file.falco_custom.rendered
   ]
+
+  set {
+    name  = "falcosidekick.enabled"
+    value = true
+  }
+
+  set {
+    name  = "falcosidekick.slack.webhookurl"
+    value = var.webhookurl
+  }
+
+  # TODO: update once knative stuff is in place
+  set {
+    name  = "falcosidekick.config.cloudevents.address"
+    value = ""
+  }
 }
 
 data "template_file" "falco_template" {
@@ -25,104 +41,4 @@ data "template_file" "falco_template" {
 
 data "template_file" "falco_custom" {
   template = file("${path.module}/templates/custom.local.yaml")
-}
-
-###################### FALCO-SIDEKICK
-resource "helm_release" "falco_sidekick" {
-
-  depends_on       = []
-  name             = "falcosidekick"
-  repository       = "https://falcosecurity.github.io/charts"
-  chart            = "falcosidekick"
-  version          = "0.5.7"
-  namespace        = "falco"
-  create_namespace = true
-  wait             = true
-  recreate_pods    = true
-  lint             = true
-  cleanup_on_fail  = true
-
-  set {
-    name  = "config.debug"
-    value = "true"
-  }
-
-  set {
-    name  = "config.customfields.cluster"
-    value = "${var.product}-${var.project_id}"
-  }
-
-  set {
-    name  = "config.openfaas.functionname"
-    value = "falco-pod-delete"
-  }
-
-  set {
-    name  = "config.slack.webhookurl"
-    value = var.webhookurl
-  }
-  dynamic "set" {
-    for_each = var.value_overrides
-    iterator = override
-    content {
-      name  = override.key
-      value = override.value
-    }
-  }
-}
-
-###################### OpenFaas DELETE-POD
-data "template_file" "openfaas" {
-  template = file("${path.module}/templates/openfaas.values.yaml")
-}
-
-resource "helm_release" "openfaas" {
-  name             = "openfaas"
-  repository       = "https://openfaas.github.io/faas-netes/"
-  chart            = "openfaas"
-  namespace        = "openfaas"
-  version          = "10.1.1"
-  create_namespace = true
-  wait             = true
-  recreate_pods    = true
-  lint             = true
-  cleanup_on_fail  = true
-
-  values = [data.template_file.openfaas.rendered]
-  depends_on = [
-    kubernetes_namespace.openfaas-fn
-  ]
-}
-
-resource "kubernetes_namespace" "openfaas-fn" {
-  metadata {
-    annotations = {
-      name = "openfaas-fn"
-    }
-
-    labels = {
-      application = "openfaas"
-    }
-    name = "openfaas-fn"
-  }
-}
-
-resource "helm_release" "openfaas_functions" {
-  name             = "openfaas-functions"
-  chart            = "${path.module}/helm/openfaas"
-  namespace        = kubernetes_namespace.openfaas-fn.id
-  create_namespace = true
-  wait             = true
-  recreate_pods    = true
-  lint             = true
-  force_update     = true
-  cleanup_on_fail  = true
-
-  depends_on = [
-    helm_release.openfaas
-  ]
-  set {
-    name  = "force_update"
-    value = timestamp()
-  }
 }
